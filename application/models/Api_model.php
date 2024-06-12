57000c775c6506d516c02a225109706d8ef599e1
PolaNabil <101729883+pola2411@users.noreply.github.com>
2024-06-12 16:23:15 +0200

    fddf

--- application/models/Api_model.php
+++ application/models/Api_model.php
@@ -63,7 +63,6 @@ class Api_model extends CI_Model
 		$user = $this->crud_model->get_user_insta($user_id);
 		// If user details are found, return the subcat field
 		if ($user !="") {
-			return 'asaf';
 			$subcat = $user['subcat'];
 			// Retrieve courses where sub_category_id matches the user's subcat
 		      // Retrieve courses where sub_category_id matches the user's subcat and group by user
